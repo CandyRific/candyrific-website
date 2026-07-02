c3b8feccb772307c9fbd1e24cec4cdfea0a8df40
knunn552 <knunn553@outlook.com>
2026-07-01 20:00:18 -0400

    Scaffolded project and began working on horizantal brand banner.

---
+++ src/components/BrandBanner.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="marquee-container">
+        <div class="marquee-track">
+            <div class="marquee-item"><a><img src="/src/assets/brand_images/marvel-logo.png"></a></div>
+            <div class="marquee-item"><a><img src="/src/assets/brand_images/marvel-logo.png"></a></div>
+            <div class="marquee-item"><a><img src="/src/assets/brand_images/marvel-logo.png"></a></div>
+            <div class="marquee-item"><a><img src="/src/assets/brand_images/marvel-logo.png"></a></div>
+            <div class="marquee-item"><a><img src="/src/assets/brand_images/marvel-logo.png"></a></div>
+
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.marquee-container {
+  overflow: hidden;
+  width: 100%;
+  background-color: #ffcc00; /* Your banner color */
+  white-space: nowrap;
+}
+
+.marquee-track {
+  display: flex;
+  width: max-content;
+  animation: scroll 15s linear infinite;
+}
+
+.marquee-track:hover {
+  animation-play-state: paused; /* Pauses when users hover */
+}
+
+.marquee-item {
+  padding: 15px 50px;
+  font-weight: bold;
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
+}
+
+
+</style>
+
+
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import BrandBanner from './components/BrandBanner.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
+    <p> This is like the layout as anything on here will appear on all pages</p>
+    <div >
+      <HelloWorld msg="You did it!" />
+      <BrandBanner />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
