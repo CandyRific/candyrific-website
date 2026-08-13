6f38ecb24b8779f0427addfd04243f71ca85bfaf
knunn552 <knunn553@outlook.com>
2026-08-13 11:53:50 -0400

    Made overflow x scroll instead of auto.

--- src/components/CarouselMobileCard.vue
+++ src/components/CarouselMobileCard.vue
@@ -26,10 +26,10 @@
 <style scoped>
 .carousel-mobile-slider{
     width: 100%;
-    overflow-x: auto;
+    overflow-x: scroll;
     overflow-y: hidden;
     display: flex;
-    /* scroll-snap-type: x mandatory; */
+    scroll-snap-type: x mandatory;
     -webkit-overflow-scrolling: touch;
 }
 
