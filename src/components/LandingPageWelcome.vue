61e1e263d1d0c9c5d76a13808ff18e0a87d3c372
knunn552 <knunn553@outlook.com>
2026-08-20 14:25:04 -0400

    Modified the type of the landing page hero image from png to webp for better file transfer.

--- src/components/LandingPageWelcome.vue
+++ src/components/LandingPageWelcome.vue
@@ -2,7 +2,7 @@
     <div class="landing-hello">
         <img src="../assets/se_design.png" class="se-design">
         <div class="landing-hello-image">
-            <img src="../assets/hello_page_image.png" alt="">
+            <img src="../assets/hello_page_image.webp" alt="">
         </div>
         <div class="landing-hello-message">  
             
